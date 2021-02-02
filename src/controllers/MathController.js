4809f4acb8d1da57609648ecd74a0b78106fac95
MateusPassosAmaral <mateus.amaral018@gmail.com>
2021-02-02 10:24:12 -0300

    conversão de códigos em formulas matemáticas

--- src/controllers/MathController.js
+++ src/controllers/MathController.js
@@ -1,36 +1,32 @@
 import mathAPI from "mathjax-node"; //pegando a biblioteca mathjax
-import base64 from 'base-64';
-mathAPI.config({
-  MathJax: {},
-});
+import base64 from "base-64";
+
 mathAPI.start();
 class MathController {
   store(request, response) {
     try {
       const { mathbody } = request.body;
-
       mathAPI.typeset(
         {
-          linebreaks: true,
+          linebreaks:false,
           math: base64.decode(mathbody),
-          format: "TeX", //the input format (TeX, inline-TeX, AsciiMath, or MathML)//FORMATOS
+          format: "AsciiMath", //the input format (TeX, inline-TeX, AsciiMath, or MathML)//FORMATOS
 
           mml: true, // or svg:true, or html:true //SAIDAS
         },
         function (data) {
 
-          if (!data.errors) {
-            return response
-              .status(200)
-              .json({
-                messasge: "Convertido com sucesso",
-                output: base64.encode(data.speakText),
 
-              });
+
+          if (!data.errors) {
+            return response.status(200).json({
+              messasge: "Convertido com sucesso",
+              output: base64.encode(data.mml),
+            });
           } else {
             return response
               .status(400)
-              .json({ erro: "erro ao tentar converter" });
+              .json({ message: "erro ao tentar converter", erro: data.errors });
           }
         }
       );
