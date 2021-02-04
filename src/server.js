7b5f2038b052b0a01b02f602753838bce77933eb
MateusPassosAmaral <mateus.amaral018@gmail.com>
2021-02-04 12:21:47 -0300

    mudanças no procfile

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const port = process.env.APP_PORT; //CASO for para frente criar arquivo .dontenv e clc a porta
+const port = process.env.APP_PORT ||3333; //CASO for para frente criar arquivo .dontenv e clc a porta
 
 app.listen(port);
